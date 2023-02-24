1fdca91b177accc37ff1c64fc269f0c6d5eea420
Victor Rutskin <Lashaka250375@gmail.com>
2023-02-24 23:20:38 +0200

    routing start and small fixes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,15 @@
+import { MainPageComponent } from './Pages/main-page/main-page.component';
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AddProductPage1Component } from './Pages/add-product-page1/add-product-page1.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'Home', component: MainPageComponent },
+  { path: 'NewListing', component: AddProductPage1Component },
 
+  // otherwise redirect to home
+  { path: '**', redirectTo: 'Home' },
+];
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
